c036861d007aa43c628a7e72753419798b335ed7
Stephen Karl Jeoffrey Hisole <skjghisole@yahoo.com>
2019-08-14 00:44:43 -0400

    working hero

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React from 'react';
-import { Grid, Typography } from '@material-ui/core'
+import { Grid } from '@material-ui/core'
 import { Hero } from './components'
-import hero from './assets/hero.jpg'
+import heroImg from './assets/hero.jpg'
 
 export default () => {
   return (
-    <Grid container direction='column' justify='center' alignItems='center'>
-      <Hero source={hero} />
-      <Grid item xs={12}>
-        <Typography>Stephen Karl Jeoffrey G. Hisole</Typography>
-      </Grid>
+    <Grid>
+      <Hero source={heroImg} />
     </Grid>
   )
 }
--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -1,12 +1,50 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Grid } from '@material-ui/core'
 
 export default (props) => {
+	const [windowScroll, setWindowScroll] = useState(window.pageYOffset/3)
+
+	const transform = () => {
+		const temp = window.pageYOffset/3
+		console.log(windowScroll)
+		setWindowScroll(temp)
+	}
+
+	useEffect(() => {
+		window.addEventListener("scroll", transform)
+	}, [])
+
 	return (
-		<Grid container>
-			<Grid item xs={12}>
-				<img src={props.source} width='100%'/>
-			</Grid>
-		</Grid>
+		<div style={{
+      backgroundImage: `url(${props.source})`,
+	    height: "90vh",
+	    maxHeight: "1000px",
+	    overflow: "hidden",
+	    position: "relative",
+	    backgroundPosition: "center center",
+	    backgroundSize: "cover",
+	    margin: "0",
+	    padding: "0",
+	    border: "0",
+	    display: "flex",
+	    alignItems: "center",
+	    transform: "translate3d(0," + windowScroll + "px,0)",
+	        "&:before": {
+      background: "rgba(0, 0, 0, 0.5)"
+    },
+    "&:after,&:before": {
+      position: "absolute",
+      zIndex: "1",
+      width: "100%",
+      height: "100%",
+      display: "block",
+      left: "0",
+      top: "0",
+      content: "''"
+    }
+		}}
+		>
+			{props.children}
+		</div>
 	)
 }
